06f4c68b8563c9333951cc084a1719e8c8b6e992
nash15963 <nash1596315963@gmail.com>
2022-05-05 17:19:01 +0800

    push

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,37 @@
-import React from 'react'
+import React ,{ useState } from 'react'
 import member from '../img/member.png'
 import '../css/login.css'
 
 const Login = () => {
     const loginPage = 
+    <div className='loginPage'>
         <form>
             <input type="text" placeholder='account'/>
             <input type="password" placeholder='password'/>
          </form>
-    const signUp = 
+    </div>
+    const signUpPage = 
+    <div className='signUpPage'>
         <form>
             <input type="text" placeholder='account'/>
             <input type="password" placeholder='password'/>
         </form>
-
+    </div>
+    const [form, setForm] = useState(loginPage)
+    const toggleMemberPage =()=>{
+        let member_page = document.querySelector(".member_page")
+        member_page.style = 'display:block'
+    }
   return (
     <div className='login'>
-        <img src={member} alt="member" />
+        <img src={member} alt="member" className='member_btn' onClick={toggleMemberPage}/>
         <div className="member_page">
             <div className="member_bar">
                 <p>sign Up</p>
                 <p>sign In</p>
                 <p>X</p>
             </div>
-            
+            {form}
         </div>
     </div>
   )
