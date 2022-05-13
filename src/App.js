45fbcdc17c4c7fa76e92b27563829878a43f15e2
nash15963 <nash1596315963@gmail.com>
2022-05-13 17:37:28 +0800

    0513 first animation done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './components/Home'
 import Game from './components/Game'
 import './styl/App.css'
 import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Discription from './components/Description'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path='/Description' element={<Discription/>}/>
       <Route path="/Game" element={<Game />} />
     </Routes>
     </BrowserRouter>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React ,{ useEffect }from 'react'
 import '../styl/Home.css'
+
+// 之後可以新增往上往下滑縮減版面
 
 
 const Home = () => {
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    console.log(position)
+    const {
+      scrollTop,
+      scrollHeight,
+      clientHeight
+    } = document.documentElement;
+  if (scrollTop + clientHeight === scrollHeight){
+    setTimeout(() => {
+      window.location.href ='./Description'
+    }, "500")
+
     
+  }
+    
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="Home">
         <div className='title-content'>
@@ -24,13 +50,16 @@ const Home = () => {
           <p>PLAY LORDLE</p>
           <p>PLAY LORDLE</p>
           <p>PLAY LORDLE</p>
+          <p>PLAY LORDLE</p>
+          <p>PLAY LORDLE</p>
+          <p>PLAY LORDLE</p>
+          <p>PLAY LORDLE</p>
           </div>
           
           <div className='bottom-title'>
             <p className='bottom-title-big'>PLAY LORDLE</p>
             <p className='bottom-title-small'>Creativity doesn’t wait for that perfect moment. It fashions its own perfect moments out of ordinary ones. ↘</p>
           </div>
-
         </div>
         
     </div>
